93af8eb8712963ea39492248ace9f610026c21d5
ldepetriss.github.io <ld5074@bard.edu>
2016-08-15 10:21:08 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,10 +5,6 @@
  */
 
 var genres = ["Hip Hop", "Ambient", "EDM", "Jazz", "Trap", "Soul"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
-var castles = ["museums", "bridges", "cities"]
-var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
-var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
 
 function choosei(lst){
     var i = Math.floor(Math.random() * lst.length);
